b0eacb99610d65705f84492a591abe1b401b104d
rpacheco <rodrigo.kpacheco@gmail.com>
2026-04-23 17:47:10 -0600

    added netlify bridge

---
+++ netlify/functions/api.js
@@ -0,0 +1,10 @@
+// netlify/functions/api.js
+const express = require("express");
+const serverless = require("serverless-http");
+const app = express();
+const router = express.Router();
+
+router.get("/", (req, res) => res.send("Hello from Node.js on Netlify!"));
+
+app.use("/.netlify/functions/api", router);
+module.exports.handler = serverless(app);
